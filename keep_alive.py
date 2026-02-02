6de89eb1187ec8467f1efedec94e9c2ad8d6b889
jubayer4599 <jubayerahmed3501@gmail.com>
2026-02-02 22:34:41 +0300

    Use environment variable for server port
    Modify the run function to use an environment variable for the port, defaulting to 8080.

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,7 +9,9 @@ def home():
     return "I am alive"
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+    # Render থেকে দেওয়া পোর্টে রান করবে, না পেলে 8080 নিবে
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
